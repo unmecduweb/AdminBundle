297beb8f1ceaeb3411a8c47f273757ae232ed862
fab <fabien@unmecduweb.com>
2017-02-27 18:12:25 +0100

    Adding sf version in type

--- Form/AbstractType.php
+++ Form/AbstractType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -23,28 +24,53 @@ class AbstractType extends SfAbstractType
         {
                 
                 
-                $gotoChoices['seeList'] = "admin.edit.goto.seeList";
-                $gotoChoices['stayHere'] = "admin.edit.goto.stayHere";
-                foreach ($options['attr']['locales'] as $locale) {
-                        $gotoChoices['otherLanguages-' . $locale] = 'admin.edit.goto.otherLanguages.' . $locale;
-                }
-                
-                $builder
-                        ->add('status', ChoiceType::class, [
-                                'label' => 'admin.edit.status',
-                                'expanded'=>true,
-                                'choices' => array(
-                                        '1' => "admin.edit.statusOn",
-                                        '0' => "admin.edit.statusOff"
-                                )
+                if (preg_match('#^2#', Kernel::VERSION)) {
+                        $gotoChoices['seeList'] = "admin.edit.goto.seeList";
+                        $gotoChoices['stayHere'] = "admin.edit.goto.stayHere";
+                        foreach ($options['attr']['locales'] as $locale) {
+                                $gotoChoices['otherLanguages-' . $locale] = 'admin.edit.goto.otherLanguages.' . $locale;
+                        }
                         
-                        ])
-                        ->add('goTo', ChoiceType::class, [
-                                'label' => 'admin.edit.goto',
-                                'mapped' => false,
-                                'choices' => $gotoChoices
-                        ]);
+                        $builder
+                                ->add('status', ChoiceType::class, [
+                                        'label' => 'admin.edit.status',
+                                        'expanded' => true,
+                                        'choices' => array(
+                                                '1' => "admin.edit.statusOn",
+                                                '0' => "admin.edit.statusOff"
+                                        )
+                                
+                                ])
+                                ->add('goTo', ChoiceType::class, [
+                                        'label' => 'admin.edit.goto',
+                                        'mapped' => false,
+                                        'choices' => $gotoChoices
+                                ]);
+                        
+                } else {
+                        $gotoChoices['seeList'] = "Retour à la liste";
+                        $gotoChoices['stayHere'] = "Rester ici";
+                        foreach ($options['attr']['locales'] as $locale) {
+                                $gotoChoices['otherLanguages-' . $locale] = 'Editer le document en ' . $locale;
+                        }
+                        $builder
+                                ->add('status', ChoiceType::class, [
+                                        'label' => 'admin.edit.status',
+                                        'expanded' => true,
+                                        'choices' => array(
+                                                "Oui" => '1',
+                                                "Non" => '0'
+                                        )
+                                
+                                ])
+                                ->add('goTo', ChoiceType::class, [
+                                        'label' => 'admin.edit.goto',
+                                        'mapped' => false,
+                                        'choices' => array_flip($gotoChoices)
+                                ]);
+                }
         }
+        
         
         
         /**
