34011ca5b80c35b54446af75536b8461a403b681
Fab <fab@fab.mweb>
2018-02-28 15:54:30 +0100

    AdminBundle deprecied for 2.8

--- Twig/TwigExtension.php
+++ Twig/TwigExtension.php
@@ -15,7 +15,6 @@ class TwigExtension extends \Twig_Extension
         
         
         private $container;
-        private $request =false;
         private $vichHelper;
         private $liipCacheHelper;
         private $requestStack = false;
@@ -25,13 +24,8 @@ class TwigExtension extends \Twig_Extension
         {
                 $this->container = $container;
                 
-                if (preg_match('#^2#', Kernel::VERSION)) {
-                        if($this->container->isScopeActive('request')) {
-                                $this->request = $this->container->get('request');
-                        }
-                }else{
-                        $this->requestStack = $requestStack->getCurrentRequest();
-                }
+                $this->requestStack = $requestStack->getCurrentRequest();
+
                 $this->liipCacheHelper = $this->container->get('liip_imagine.cache.manager');
 //                if ($this->container->get('router.request_context')->get('_route') !== null) {
 //                        $this->locale = $this->container->get('request')->getLocale();
@@ -71,8 +65,8 @@ class TwigExtension extends \Twig_Extension
         {
                 $ret = array();
                 $langs = explode('|',$this->container->getParameter('locales'));
-                $currentRoute = $this->request->attributes->get('_route');
-                $params = array_merge((array)$this->request->attributes->get('_route_params'), $_GET);
+                $currentRoute = $this->requestStack->attributes->get('_route');
+                $params = array_merge((array)$this->requestStack->attributes->get('_route_params'), $_GET);
 
 //                $curLocale = $this->getLocale();
                 if ($currentRoute) {
